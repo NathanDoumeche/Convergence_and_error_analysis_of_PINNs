6f7337ef132ebcb97d655f86979cc759a90da108
Nathan Doumeche <nathan.doumeche@gmail.com>
2023-04-13 11:16:17 +0200

    First version of PINN

--- Expes/PINNs_2d.py
+++ Expes/PINNs_2d.py
@@ -9,6 +9,7 @@ from torch.autograd import Variable
 
 torch.manual_seed(42)
 
+
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
@@ -17,52 +18,67 @@ class NeuralNetwork(nn.Module):
     def forward(self, x):
         xGrad = Variable(x, requires_grad = True)
         r = self.dense(xGrad)
+
+        # Gradient computations
         gradientR = torch.autograd.grad(r, xGrad, torch.ones_like(r), create_graph=True)
         self.gradientR = gradientR[0]
         self.gradient2R1 = torch.autograd.grad(gradientR[0][:,0], xGrad, torch.ones_like(gradientR[0][:,0]), create_graph=True)[0]
         self.gradient2R2 = torch.autograd.grad(gradientR[0][:,1], xGrad, torch.ones_like(gradientR[0][:,1]), create_graph=True)[0]
         return r
 
-def mse(y):
-    return nn.functional.mse_loss(y, torch.zeros(y.size()))
+def mean_squared(vector):
+    return nn.functional.mse_loss(vector, torch.zeros(vector.size()))
 
-def lossGrad(pred, target, gradientNN, gradient2_1NN, gradient2_2NN, lambda_t, lambda_d):
-    constraint_advection = gradientNN[:,0]-gradientNN[:,1]
-    sobolev0 = pred
-    sobolev1 = gradientNN
-    sobolev2_1 = gradient2_1NN
-    sobolev2_2 = gradient2_2NN
-    return lambda_d*nn.functional.mse_loss(pred, target), \
-        mse(constraint_advection), \
-        lambda_t * (mse(sobolev0)+mse(sobolev1)+mse(sobolev2_1)+mse(sobolev2_2))
+def sobolev_regularization(model, n_r, pred):
+    sobolev0 = pred[0:n_r,]
+    sobolev1 = model.gradientR[0:n_r,]
+    sobolev2_1 = model.gradient2R1[0:n_r,]
+    sobolev2_2 = model.gradient2R2[0:n_r,]
+    return mean_squared(sobolev0)+mean_squared(sobolev1)+mean_squared(sobolev2_1)+mean_squared(sobolev2_2)
 
 
-def train(train_dataloader, val_dataloader, model, loss_fn, optimizer, lambda_t, lambda_d):
+def advection_constraint(model, n_r):
+    return mean_squared(model.gradientR[0:n_r,0]-model.gradientR[0:n_r,1])
+
+
+def boundary_initial_conditions(pred, n_r, n_e, h):
+    return nn.functional.mse_loss(pred[n_r:n_r+n_e,], h)
+
+
+def lossGrad(model, x_r, x_e, h, x, y):
+    x_tot = torch.cat((x_r, x_e, x))
+    pred = model(x_tot)
+    return nn.functional.mse_loss(pred[len(x_r)+len(x_e):], y), boundary_initial_conditions(pred, len(x_r), len(x_e), h),\
+        advection_constraint(model, len(x_r)), sobolev_regularization(model, len(x_r), pred)
+
+
+def train(train_loader_r, train_loader_e, train_loader_data, val_loader, model, loss_fn, optimizer, lambda_t, lambda_d):
+    batch_number = len(train_loader_r)
     model.train()
     loss_trainAcc = []
-    loss_trainCstr = []
 
-    for batch, X in enumerate(train_dataloader):
-        x = X[:,0:2].reshape(-1,2)
-        y = X[:,2].reshape(-1,1)
+    train_r = iter(train_loader_r)
+    train_e = iter(train_loader_e)
+    train_XY = iter(train_loader_data)
 
-        # Compute prediction error
-        pred = model(x)
-        loss = loss_fn(pred, y, model.gradientR, model.gradient2R1, model.gradient2R2, lambda_t, lambda_d)
-        lossTot = loss[0]+loss[1]+loss[2]
+    for batch in range(batch_number):
+        x_r, XeH, XY = next(train_r), next(train_e), next(train_XY)
+        x_e, h = XY[:,0:2].reshape(-1,2), XY[:,2].reshape(-1,1)
+        x, y = XY[:,0:2].reshape(-1,2), XY[:,2].reshape(-1,1)
 
         # Backpropagation
         optimizer.zero_grad()
+        loss = loss_fn(model, x_r, x_e, h, x, y)
+        lossTot = lambda_d * loss[0] + loss[1] + loss[2] + lambda_t * loss[3]
         lossTot.backward(retain_graph=True)
         optimizer.step()
 
-        loss_trainAcc.append(loss[0].item()/lambda_d)
-        loss_trainCstr.append(loss[1].item())
+        loss_trainAcc.append(loss[0].item())
     loss_mean_train = np.mean(np.array(loss_trainAcc))
 
     model.eval()
     lossValAcc = []
-    for batch, X in enumerate(val_dataloader):
+    for batch, X in enumerate(val_loader):
         x = X[:,0:2].reshape(-1,2)
         y = X[:,2].reshape(-1,1)
         # Compute prediction error
@@ -72,33 +88,44 @@ def train(train_dataloader, val_dataloader, model, loss_fn, optimizer, lambda_t,
     return loss_mean_train, loss_mean_val
 
 
-def simulation(n):
-    # HYPERPARAMETERS
-    epochs = 50
-    batch_size = n//10
-    noise = 0.1
-    lr = 1e-4
-    model_name = "model_deep_pinn"
+def simulation(n, n_e, n_r, n_val):
+    # Hyperparameters
+    epochs, batch_number, lr = 50, 10, 10**(-4)
 
     # PINNs hyperparameter
-    weight_decay = n**(-1/2)
+    weight_decay = n_r**(-1/2)
     lambda_t = 0.1*(np.log(n))**(-1)
     lambda_d = (n**(1/2)) * lambda_t * 10
 
+    #Model
+    noise = 0.1
+
     # Learning dataset: 0 < t,x < 1
-    train_x = torch.rand(n,2)
-    train_y = (torch.exp(train_x[:,0]-train_x[:,1])).reshape(-1,1) + noise*torch.rand(n,1)
-    train_dataset = torch.cat((train_x, train_y), dim=1)
+    train_Xr = torch.rand(n_r,2)
+
+    initial_points = torch.cat((torch.zeros((n_e//2,1)), torch.rand(n_e//2,1)), dim=1)
+    initial_values = (torch.exp(-initial_points[:,1])).reshape(-1,1)
+    initial_conditions = torch.cat((initial_points, initial_values), dim=1)
+
+    boundary_points = torch.cat((torch.rand(n_e//2,1), torch.zeros((n_e//2,1))), dim=1)
+    boundary_values = (torch.exp(boundary_points[:,0])).reshape(-1,1)
+    boundary_conditions = torch.cat((boundary_points, boundary_values), dim=1)
+    train_Xe = torch.cat((initial_conditions, boundary_conditions))
+
+    train_X = torch.rand(n, 2)
+    train_y = (torch.exp(train_X[:,0]-train_X[:,1])).reshape(-1,1) + noise*torch.rand(n,1)
+    train_data = torch.cat((train_X, train_y), dim=1)
 
     #Validation dataset: 0 < t,x < 1
-    m = 100000
-    val_x = torch.rand(m, 2)
+    val_x = torch.rand(n_val, 2)
     val_y = (torch.exp(val_x[:,0]-val_x[:,1])).reshape(-1,1)
     val_dataset = torch.cat((val_x, val_y), dim=1)
 
-    # Create data loaders.
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
-    val_dataloader = DataLoader(val_dataset, batch_size=m)
+    # Create data loaders
+    train_loader_r = DataLoader(train_Xr, batch_size=n_r//batch_number)
+    train_loader_e = DataLoader(train_Xe, batch_size=n_e//batch_number)
+    train_loader_data = DataLoader(train_data, batch_size=n//batch_number)
+    val_loader = DataLoader(val_dataset, batch_size=n_val)
 
     model = NeuralNetwork()
     loss_fn = lossGrad
@@ -106,11 +133,10 @@ def simulation(n):
 
     train_loss = []
     val_loss = []
-    if not os.path.exists('checkpoints'):
-        os.makedirs('checkpoints')
     for t in range(epochs):
-        print(f"\nEpoch {t+1}\n-------------------------------")
-        loss_train, loss_val = train(train_dataloader, val_dataloader, model, loss_fn, optimizer, lambda_t, lambda_d)
+        print(f"Epoch {t+1}")
+        loss_train, loss_val = train(train_loader_r, train_loader_e, train_loader_data, val_loader,
+                                     model, loss_fn, optimizer, lambda_t, lambda_d)
         train_loss.append(loss_train)
         val_loss.append(loss_val)
 
@@ -123,10 +149,14 @@ def simulation(n):
 
     return np.log(np.array(val_loss))[-1]
 
+
 if __name__ == "__main__":
     list_n, val_loss=[], []
     for order_of_magnitude in np.arange(1, 5, 0.5):
-        print("Training model for n = "+str(10**order_of_magnitude))
-        val_loss.append(simulation(int(10**order_of_magnitude)))
-        list_n.append(np.log(int(10**order_of_magnitude)))
+        n, n_e, n_val = int(10**order_of_magnitude), 10**5, 10**5
+        n_r = n_e
+
+        print("Training model for n = "+str(n))
+        val_loss.append(simulation(n, n_e, n_r, n_val))
+        list_n.append(np.log(n))
     pd.DataFrame({"log(n)": list_n, "log(Validation_loss)": val_loss}).to_csv(os.path.join("Outputs_PINNs_2d", "validation_loss.csv"))
